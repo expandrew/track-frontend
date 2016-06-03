3ed12992b414590d73645fb4fd7cde6c5dc848ea
Andrew Westling <a@andrewwestling.com>
2016-06-03 18:52:04 -0400

    (fix some residual issues with app rename and Jade removal)

--- angular/gulp/build.js
+++ angular/gulp/build.js
@@ -19,7 +19,7 @@ gulp.task('partials', function () {
       quotes: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'angular',
+      module: 'track-angular',
       root: 'app'
     }))
     .pipe(gulp.dest(conf.paths.tmp + '/partials/'));
