f9884230ecc6525cb0d8dba1f458c3d1a91ee340
Andrew Westling <a@andrewwestling.com>
2016-06-03 18:34:45 -0400

    (add $resources to /app/core/resources folder)

---
+++ angular/src/app/core/resources/item.resource.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('track-angular')
+    .factory('Item', function($resource) {
+
+      return $resource('//localhost:5000/api/v1/items/:id');
+    });
+
+})();
--- angular/src/app/core/resources/user.resource.js
+++ angular/src/app/core/resources/user.resource.js
@@ -3,11 +3,6 @@
 
   angular
     .module('track-angular')
-    .factory('Item', function($resource) {
-
-      return $resource('//localhost:5000/api/v1/items/:id');
-    })
-
     .factory('User', function($resource) {
 
       return $resource('//localhost:5000/api/v1/users/:id');
